c7bb8fbf29f8553d7e0ac11cd373b1f7ae17268e
Johan 't Hart <johan.thart@anywhere365.net>
2021-10-30 21:32:38 +0200

    Create basic particle 'engine'

--- src/particles/particles.tsx
+++ src/particles/particles.tsx
@@ -1,40 +1,61 @@
-import React from 'react';
-import * as THREE from "three";
-
-const renderScene = (node: HTMLElement | null) => {
-    if (!node) {
-        return;
-    }
-
-    // Init three
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    node.appendChild( renderer.domElement );
-    camera.position.z = 5;
-
-
-    // Add cube
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-
-    // Animate
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
-};
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
 function Particles() {
+
+  const node = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    // Init three
+    const currentNode = node.current;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    currentNode?.appendChild(renderer.domElement);
+    camera.position.z = 5;
+
+    // create the particle variables
+    const vertices = [];
+
+    for ( let i = 0; i < 100000; i ++ ) {
+
+        const x = THREE.MathUtils.randFloatSpread( 2000 );
+        const y = THREE.MathUtils.randFloatSpread( 2000 );
+        const z = THREE.MathUtils.randFloatSpread( 2000 );
+
+        vertices.push( x, y, z );
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+    const material = new THREE.PointsMaterial( { color: 0x888888 } );
+
+    const points = new THREE.Points( geometry, material );
+
+    scene.add( points );
+
+    // Animate
+    let stopAnimation = false;
+
+    const animate = () => {
+      if (stopAnimation) {
+        return;
+      }
+      requestAnimationFrame( animate );
+      renderer.render( scene, camera );
+    };
+
+    animate();
+    return () => {
+      stopAnimation = true;
+      currentNode?.removeChild(renderer.domElement);
+    };
+  });
+
   return (
-    <div ref={renderScene}>
+    <div ref={node}>
     </div>
   );
 }
