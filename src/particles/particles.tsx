f2c9e31c85c0899fffdbdec2520d195ae5bbefe4
Johan 't Hart <johan.thart@anywhere365.net>
2021-10-31 23:31:04 +0100

    Be able to reset (r) and change mass (+,-)

--- src/particles/particles.tsx
+++ src/particles/particles.tsx
@@ -13,43 +13,51 @@ function Particles() {
     const particles: number[] = [];
     const dParticles: number[] = [];
     const particleColors: number[] = [];
-    const color = new THREE.Color();
-    const zAxis = new THREE.Vector3(0, 0, 1);
-    const xAxis = new THREE.Vector3(1, 0, 0);
 
-    const particleCount = 100000;
     const speed = 0.1;
-
-    for ( let i = 0; i < particleCount; i ++ ) {
-
-      const perc = i / particleCount;
-
-      // const x = THREE.MathUtils.randFloatSpread( 2000 );
-      // const y = THREE.MathUtils.randFloatSpread( 2000 );
-      // const z = THREE.MathUtils.randFloatSpread( 2000 );
-      const x = 0;
-      const y = 0;
-      const z = 0;
-
-      particles.push( x, y, z );
-
-      //const dp = new THREE.Vector3(perc * speed, 0, 0);
-      const dp = new THREE.Vector3(((perc * 3 | 0) / 3 + .1) * speed, 0, 0);
-
-      // dp.applyAxisAngle(zAxis, perc * Math.PI * 40);
-      // dp.applyAxisAngle(xAxis, perc * Math.PI * 2);
-      dp.applyAxisAngle(zAxis, THREE.MathUtils.randFloatSpread(Math.PI * 2));
-      // dp.applyAxisAngle(xAxis, THREE.MathUtils.randFloatSpread(Math.PI * 2));
-
-      dParticles.push(dp.x, dp.y, dp.z);
-
-      color.setHSL(perc, 1.0, 0.5);
-      particleColors.push(color.r, color.g, color.b);
-    }
-
     const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( particles, 3 ) );
-    geometry.setAttribute( 'color', new THREE.Float32BufferAttribute(particleColors, 3));
+
+    const reset = () => {
+      const color = new THREE.Color();
+      const zAxis = new THREE.Vector3(0, 0, 1);
+      const xAxis = new THREE.Vector3(1, 0, 0);
+      const particleCount = 100000;
+
+      particles.length = 0;
+      dParticles.length = 0;
+      particleColors.length = 0;
+      for ( let i = 0; i < particleCount; i ++ ) {
+
+        const perc = i / particleCount;
+
+        // const x = THREE.MathU tils.randFloatSpread( 2000 );
+        // const y = THREE.MathUtils.randFloatSpread( 2000 );
+        // const z = THREE.MathUtils.randFloatSpread( 2000 );
+        const x = 0;
+        const y = 0;
+        const z = 0;
+
+        particles.push( x, y, z );
+
+        //const dp = new THREE.Vector3(perc * speed, 0, 0);
+        const dp = new THREE.Vector3(((perc * 3 | 0) / 3 + .1) * speed, 0, 0);
+
+        // dp.applyAxisAngle(zAxis, perc * Math.PI * 40);
+        // dp.applyAxisAngle(xAxis, perc * Math.PI * 2);
+        dp.applyAxisAngle(zAxis, THREE.MathUtils.randFloatSpread(Math.PI * 2));
+        // dp.applyAxisAngle(xAxis, THREE.MathUtils.randFloatSpread(Math.PI * 2));
+
+        dParticles.push(dp.x, dp.y, dp.z);
+
+        color.setHSL(perc, 1.0, 0.5);
+        particleColors.push(color.r, color.g, color.b);
+      }
+      geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( particles, 3 ) );
+      geometry.setAttribute( 'color', new THREE.Float32BufferAttribute(particleColors, 3));
+    };
+
+    reset();
+
 
     const material = new THREE.PointsMaterial( { size: 1, vertexColors: true } );
     //const material = new THREE.PointsMaterial( { color: 0xD08010, size: 1 } );
@@ -59,8 +67,8 @@ function Particles() {
     scene.add( points );
 
     // Me
-    const me = (() => {
-      const geometry = new THREE.SphereGeometry(1, 32, 32);
+    const createMe = (mass: number) => {
+      const geometry = new THREE.SphereGeometry(Math.pow(mass, 1/3), 32, 32);
       const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
       const mesh = new THREE.Mesh( geometry, material );
       mesh.material.color.setRGB(1,1,1);
@@ -69,19 +77,30 @@ function Particles() {
         material,
         mesh
       };
-    })();
+    };
+    let mass = 1;
+    let me = createMe(mass);
 
     scene.add(me.mesh);
 
     // Keyboard handling
     let push = true;
     addKeyHandler(' ', _ => push = !push);
+    addKeyHandler('r', reset);
+    const multMass = (factor: number) => () => {
+      mass *= factor;
+      scene.remove(me.mesh);
+      me = createMe(mass);
+      scene.add(me.mesh);
+    };
+    addKeyHandler('=', multMass(1.5));
+    addKeyHandler('+', multMass(1.5));
+    addKeyHandler('-', multMass(0.75));
 
     const mousePlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
 
     // Animate
     const bound = 500;
-    const mass = 1;
     return timeDiff => {
 
       const mePos = mouseRay.ray.intersectPlane(mousePlane, me.mesh.position);
